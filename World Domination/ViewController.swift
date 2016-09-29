4f8d84acf6022a5d378a379a06117900bb524f90
Travis Patton <Travis_Patton@tests-macbook-air.local>
2016-09-29 06:48:43 -0700

    Initial Commit

---
+++ World Domination/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  World Domination
+//
+//  Created by Travis Patton on 9/29/16.
+//  Copyright © 2016 Travis Patton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
